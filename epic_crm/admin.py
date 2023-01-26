baad4e24dfc93715ad40cb89f1ae7798aa2282c6
antoine <kronek.antoine@gmail.com>
2023-01-26 10:43:23 +0100

    changes for user creation on admin console

--- epic_crm/admin.py
+++ epic_crm/admin.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.admin import UserAdmin 
 
 from .models import User, Client, Contract, Event
 
 @admin.register(User)
-class UserAdmin(BaseUserAdmin):
+class CustomUserAdmin(UserAdmin):
     fieldsets = (
         (None, {'fields': ('email', 'password','first_name', 'last_name', 'team', 'phone',)}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser',)}),
+    )
+    add_fieldsets = (
+        (None, {'fields': ('email', 'password1', 'password2', 'first_name', 'last_name', 'team', 'phone',)}),
         ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser',)}),
     )
     list_display = ('email', 'first_name', 'last_name', 'team', 'is_staff', 'is_superuser',)
@@ -17,19 +21,19 @@ class UserAdmin(BaseUserAdmin):
 @admin.register(Client)
 class ClientAdmin(admin.ModelAdmin):
     fieldsets = (
-        (None, {'fields': ('first_name', 'last_name', 'company', 'phone', 'status', 'sales_contact',)}),
+        (None, {'fields': ('first_name', 'last_name', 'email', 'company', 'phone', 'status', 'sales_contact',)}),
     )
-    list_display = ('first_name', 'last_name', 'company', 'phone', 'status', 'sales_contact',)
+    list_display = ('first_name', 'last_name', 'email', 'company', 'phone', 'status', 'sales_contact',)
     list_filter = ('company', 'sales_contact',)
     search_fields = ('company', 'sales_contact',)
-    ordering = ('company',)
+    ordering = ('company', 'last_name')
 
 @admin.register(Contract)
 class ContractAdmin(admin.ModelAdmin):
     fieldsets = (
-        (None, {'fields': ('amount', 'balance', 'status', 'client_contact', 'sales_contact',)}),
+        (None, {'fields': ('reference', 'amount', 'balance', 'status', 'client_contact', 'sales_contact',)}),
     )
-    list_display = ('amount', 'balance', 'creation_date', 'last_update', 'status', 'client_contact', 'sales_contact',)
+    list_display = ('reference', 'amount', 'balance', 'creation_date', 'last_update', 'status', 'client_contact', 'sales_contact',)
     list_filter = ('client_contact', 'sales_contact',)
     search_fields = ('client_contact', 'sales_contact',)
     ordering = ('client_contact',)
@@ -38,9 +42,9 @@ class ContractAdmin(admin.ModelAdmin):
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
     fieldsets = (
-        (None, {'fields': ('name', 'location', 'status', 'contract', 'support_contact',)}),
+        (None, {'fields': ('reference', 'name', 'location', 'status', 'contract', 'support_contact',)}),
     )
-    list_display = ('name', 'location', 'creation_date', 'last_update', 'status', 'contract', 'support_contact',)
+    list_display = ('reference', 'name', 'location', 'creation_date', 'last_update', 'status', 'contract', 'support_contact',)
     list_filter = ('contract', 'support_contact',)
     search_fields = ('contract', 'support_contact',)
     ordering = ('contract',)
--- epic_crm/models.py
+++ epic_crm/models.py
@@ -27,12 +27,16 @@ class Client(models.Model):
     company = models.CharField(max_length=255)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    email = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, unique=True)
     phone = models.CharField(max_length=255, blank=True, null=True)
     status = models.BooleanField(default=False, verbose_name='signed')
     sales_contact = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
 
+    def __str__(self):
+        return self.email
+
 class Contract(models.Model):
+    reference = models.CharField(max_length=25, unique=True)
     amount = models.DecimalField(max_digits=7, decimal_places=2)
     balance = models.DecimalField(max_digits=7, decimal_places=2)
     creation_date = models.DateTimeField(auto_now_add=True)
@@ -41,7 +45,11 @@ class Contract(models.Model):
     client_contact = models.ForeignKey(to=Client, on_delete=models.SET_NULL, null=True)
     sales_contact = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
 
+    def __str__(self):
+        return self.reference
+
 class Event(models.Model):
+    reference = models.CharField(max_length=25, unique=True)
     name = models.CharField(max_length=255)
     location = models.CharField(max_length=255)
     creation_date = models.DateTimeField(auto_now_add=True)
@@ -49,3 +57,6 @@ class Event(models.Model):
     status = models.BooleanField(default=False, verbose_name='completed')
     contract = models.ForeignKey(to=Contract, on_delete=models.SET_NULL, null=True)
     support_contact = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.reference
